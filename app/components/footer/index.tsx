1dda50091efb3b05872816eead079ee3b2dad1e4
rafaelpires010 <rafapires2210@gmail.com>
2024-04-22 20:08:22 -0300

    corrigindo um detalhe na div que ficou com erro

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
         Made with
         <IoMdHeart size={13} className="text-emerald-500" />
         by
-        <strong className="font-medium">Gabriel Borges</strong>
+        <strong className="font-medium">Rafael Pires</strong>
       </span>
     </footer>
   )
